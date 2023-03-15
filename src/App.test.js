6d798a9a9d4596ea5611ed9118433763da814acc
Naiara Abaroa <naiaraabaroa@gmail.com>
2023-03-15 09:29:42 +0100

    Deploying to custom domain

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Email us to hey@off.limited/i);
   expect(linkElement).toBeInTheDocument();
 });
