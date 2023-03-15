3225841a9f28b6f6dde308a3c851cd19377b5174
Naiara Abaroa <naiaraabaroa@gmail.com>
2023-03-15 09:30:10 +0100

    Updates

---
+++ precache-manifest.5ad0ee14b416d507f98bd404d16e544d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9008e9cf65670985359a",
+    "url": "/static/css/main.ecc9ea8a.chunk.css"
+  },
+  {
+    "revision": "9008e9cf65670985359a",
+    "url": "/static/js/main.9008e9cf.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "d5d59460a2f889c3b36af5a3a7df90fe",
+    "url": "/static/media/index-2680.d5d59460.png"
+  },
+  {
+    "revision": "2c9d0c7e711bc911f9bfe83f6add75bb",
+    "url": "/index.html"
+  }
+];
